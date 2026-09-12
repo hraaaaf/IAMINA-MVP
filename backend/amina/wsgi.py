f1d00a94e49e803822c905c863eb5094f8518ddf
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-12 15:48:31 +0100

    chore(infra): select Vercel production settings in WSGI

--- backend/amina/wsgi.py
+++ backend/amina/wsgi.py
@@ -5,6 +5,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amina.settings')
+settings_module = "amina.vercel_settings" if os.environ.get("VERCEL") == "1" else "amina.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
