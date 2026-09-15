eeafae6ba887d31b601faf01cf154c1d87f4cd23
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-15 14:34:37 +0100

    chore(nutrition): rebuild B15 on current main

--- frontend/test/features/food_pictogram_batch15_visual_contract_test.dart
+++ frontend/test/features/food_pictogram_batch15_visual_contract_test.dart
@@ -13,17 +13,18 @@ import 'package:amina/features/journal/widgets/food_pictogram_painter_batch11.da
 import 'package:amina/features/journal/widgets/food_pictogram_painter_batch12.dart';
 import 'package:amina/features/journal/widgets/food_pictogram_painter_batch13.dart';
 import 'package:amina/features/journal/widgets/food_pictogram_painter_batch14.dart';
+import 'package:amina/features/journal/widgets/food_pictogram_painter_batch15.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
-  test('fourteen certified V3 batches remain pairwise-disjoint baseline 322',(){
-    final fullBatches=<Set<String>>[codeFoodPictogramIds,codeFoodPictogramBatch2Ids,codeFoodPictogramBatch3Ids,codeFoodPictogramBatch4Ids,codeFoodPictogramBatch5Ids,codeFoodPictogramBatch6Ids,codeFoodPictogramBatch7Ids,codeFoodPictogramBatch8Ids,codeFoodPictogramBatch9Ids,codeFoodPictogramBatch10Ids,codeFoodPictogramBatch11Ids,codeFoodPictogramBatch12Ids,codeFoodPictogramBatch13Ids];
+  test('fifteen pictogram batches cover the complete 346-item catalog',(){
+    final fullBatches=<Set<String>>[codeFoodPictogramIds,codeFoodPictogramBatch2Ids,codeFoodPictogramBatch3Ids,codeFoodPictogramBatch4Ids,codeFoodPictogramBatch5Ids,codeFoodPictogramBatch6Ids,codeFoodPictogramBatch7Ids,codeFoodPictogramBatch8Ids,codeFoodPictogramBatch9Ids,codeFoodPictogramBatch10Ids,codeFoodPictogramBatch11Ids,codeFoodPictogramBatch12Ids,codeFoodPictogramBatch13Ids,codeFoodPictogramBatch15Ids];
     for(final batch in fullBatches){expect(batch.length,24);}
     expect(codeFoodPictogramBatch14Ids.length,10);
-    final batches=<Set<String>>[...fullBatches,codeFoodPictogramBatch14Ids];
+    final batches=<Set<String>>[codeFoodPictogramIds,codeFoodPictogramBatch2Ids,codeFoodPictogramBatch3Ids,codeFoodPictogramBatch4Ids,codeFoodPictogramBatch5Ids,codeFoodPictogramBatch6Ids,codeFoodPictogramBatch7Ids,codeFoodPictogramBatch8Ids,codeFoodPictogramBatch9Ids,codeFoodPictogramBatch10Ids,codeFoodPictogramBatch11Ids,codeFoodPictogramBatch12Ids,codeFoodPictogramBatch13Ids,codeFoodPictogramBatch14Ids,codeFoodPictogramBatch15Ids];
     for(var i=0;i<batches.length;i++){for(var j=i+1;j<batches.length;j++){expect(batches[i].intersection(batches[j]),isEmpty);}}
     final union=batches.expand((batch)=>batch).toSet();
-    expect(union.length,322);
-    expect(mealFoodCatalog.map((item)=>item.id).toSet().containsAll(union),isTrue);
+    expect(union.length,346);
+    expect(union,mealFoodCatalog.map((item)=>item.id).toSet());
   });
 }
