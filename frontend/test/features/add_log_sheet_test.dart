92ef9f378d6cc53b33d3a0ec91109bdbe40da923
hraaaaf <123034021+hraaaaf@users.noreply.github.com>
2026-09-25 03:02:31 +0100

    test(journal): await voice stop callback deterministically

--- frontend/test/features/add_log_sheet_test.dart
+++ frontend/test/features/add_log_sheet_test.dart
@@ -195,7 +195,9 @@ void main() {
         audio.add(Uint8List.fromList(<int>[1, 2, 3, 4]));
         await tester.pump();
         await tester.tap(voiceButton);
-        await tester.pump();
+        for (var i = 0; i < 20 && transcribedBytes == null; i++) {
+          await tester.pump(const Duration(milliseconds: 10));
+        }
 
         expect(transcribedBytes, isNotNull);
         expect(transcribedBytes, hasLength(4));
